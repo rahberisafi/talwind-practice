3b6bc108aec8e9ffe45a0ea66629b939202f7149
rahberisafi <rahberisafi@gmail.com>
2021-04-07 15:07:37 +0600

    talwind pratice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Dropdown from './components/Dropdown';
+import About from './pages/About';
+import Menu from './pages/Menu';
+import Home from './pages';
+import Footer from './components/Footer';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+   setIsOpen(!isOpen)
+  }
+
+  useEffect(() => {
+   const hideMenu = () => {
+     if (window.innerWidth > 768 && isOpen) {
+       setIsOpen(false);
+       console.log('its ok');
+     }
+   };
+
+   window.addEventListener('resize', hideMenu);
+
+   return () => {
+     window.removeEventListener('resize', hideMenu);
+   };
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <>
+      <Navbar toggle={toggle} />
+      <Dropdown isOpen={isOpen} toggle={toggle} />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/menu' component={Menu} />
+        <Route path='/about' component={About} />
+      </Switch>
+      <Footer />
+     </>
   );
 }
 
